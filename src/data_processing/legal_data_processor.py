a7df7bd1e2b612a17076c319cf356d4e80ab7e02
ratniel <ratniel.kokane@nitorinfotech.com>
2024-10-29 16:54:48 +0530

    remove jupyter notebooks

--- src/data_processing/legal_data_processor.py
+++ src/data_processing/legal_data_processor.py
@@ -1,5 +1,5 @@
-from typing import List, Optional, Dict
-from pydantic import BaseModel, Field
+from typing import List, Optional
+from pydantic import BaseModel
 
 system_prompt = """
 # Summary of [Document Title]
